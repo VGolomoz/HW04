7cd3177e7fc4bc80a8cfd9ae060c63eed13a6ac1
unknown <gworkout@ukr.net>
2019-07-07 15:46:17 +0300

    HW04 Update#1

--- src/main/java/HW04/StreamUtils.java
+++ src/main/java/HW04/StreamUtils.java
@@ -1,14 +1,15 @@
 package HW04;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
+import java.util.stream.*;
 
 public class StreamUtils {
 
-    public static String getStringNames(List<String> names) {
+    public static String getStringNamesByOddNumbering(List<String> names) {
 
         AtomicInteger index = new AtomicInteger(-1);
 
@@ -27,14 +28,39 @@ public class StreamUtils {
                 .collect(Collectors.toList());
     }
 
-    public static ArrayList<String> getElementsListSeparateByComma(List<String> list) {
+    public static List<String> getElementsListSeparateByComma(List<String> list) {
 
-        return (ArrayList<String>) list
+        return list
                 .stream()
                 .flatMap((p) -> Arrays.asList(p.split(",")).stream())
+                .map((p) -> p.trim())
                 .collect(Collectors.toList());
-
     }
 
 
+    public static LongStream generatorRandoms(long seed, long a, long c, long m) {
+
+        return LongStream.iterate(seed, n -> (a * n + c) % m);
+    }
+
+    public static <T>Stream<T> mixElementsFromTwoStreamsOnyByOne(Stream<T> streamA, Stream<T> streamB) {
+
+        List<T> result = new ArrayList<>();
+        List<T> listA = streamA.collect(Collectors.toList());
+        List<T> listB = streamB.collect(Collectors.toList());
+
+        for (int i = 0; i < listA.size() && i < listB.size(); i++) {
+            result.add(listA.get(i));
+            result.add(listB.get(i));
+        }
+
+        return result.stream();
+    }
+
+    public static <T>List<T> mixElementsFromTwoStreamsInOneArray(Stream<T> streamA, Stream<T> streamB) {
+
+        List<T> result = Stream.concat(streamA, streamB).collect(Collectors.toList());
+
+        return result;
+    }
 }
--- src/test/java/StreamUtilsTest.java
+++ src/test/java/StreamUtilsTest.java
@@ -2,6 +2,9 @@ import HW04.StreamUtils;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 
@@ -11,7 +14,7 @@ public class StreamUtilsTest {
     @Test
     public void shouldGetStringWithNamesWithOddNumbering() {
         assertEquals("1. Ivan, 3. Arnold, 5. Robert, 7. Dakota, 9. Mike, 11. Leonid",
-                StreamUtils.getStringNames(Arrays.asList("Ivan", "Arnold", "Robert", "Dakota", "Mike", "Leonid")));
+                StreamUtils.getStringNamesByOddNumbering(Arrays.asList("Ivan", "Arnold", "Robert", "Dakota", "Mike", "Leonid")));
     }
 
     @Test
@@ -24,6 +27,25 @@ public class StreamUtilsTest {
     public void shouldGetElementsListSeparateByComma() {
         assertEquals(Arrays.asList("1", "2", "0", "4", "5"),
                 StreamUtils.getElementsListSeparateByComma(Arrays.asList("1, 2, 0", "4, 5")));
+    }
+
+    @Test
+    public void shouldGetRandomsNumbersAccordingToCondition() {
+        LongStream result = StreamUtils.generatorRandoms(137, 6, 17, 149);
+        final String expected = "[137, 94, 134, 76, 26, 24, 12, 89, 104, 45]";
+        assertEquals(expected, result.limit(10).boxed().collect(Collectors.toList()).toString());
+    }
+
+    @Test
+    public void shouldGetOneStreamWithMixedElementsOneByOneFromTwoStreams() {
+        assertEquals(Arrays.asList("1", "2", "3", "4", "5", "6"),
+                StreamUtils.mixElementsFromTwoStreamsOnyByOne(Stream.of("1", "3", "5"), Stream.of("2", "4", "6")).collect(Collectors.toList()));
+    }
+
+    @Test
+    public void shouldGetOneStreamWithElementsFromTwoStreams() {
+        assertEquals(Arrays.asList("1", "3", "5", "7", "9", "2", "4", "6"),
+                StreamUtils.mixElementsFromTwoStreamsInOneArray(Stream.of("1", "3", "5", "7", "9"), Stream.of("2", "4", "6")));
     }
 
 
